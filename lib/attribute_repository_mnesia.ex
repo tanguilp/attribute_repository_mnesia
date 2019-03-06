d2bbeb158fd4c365d5be7b732bc133f8a4fbe391
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-03-06 20:11:39 +0300

    Implemented write callback

--- lib/attribute_repository_mnesia.ex
+++ lib/attribute_repository_mnesia.ex
@@ -3,16 +3,88 @@ defmodule AttributeRepositoryMnesia do
   Documentation for AttributeRepositoryMnesia.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  @behaviour AttributeRepository.Install
+  @behaviour AttributeRepository.Write
 
-      iex> AttributeRepositoryMnesia.hello()
-      :world
+  @impl AttributeRepository.Install
 
-  """
-  def hello do
-    :world
+  def install(run_opts, init_opts) do
+    :mnesia.stop()
+
+    :mnesia.create_schema([node()])
+
+    :mnesia.start()
+
+    case :mnesia.create_table(run_opts[:instance], [
+      attributes: [:key,
+                   :attrs,
+                   :created,
+                   :last_modified,
+                   :history
+      ]
+    ] ++ (init_opts[:mnesia_config] || [])) do
+      {:atomic, :ok} ->
+        Logger.debug("#{__MODULE__}: created table for instance #{run_opts[:instance]}")
+
+        :ok
+
+      {:aborted, reason} ->
+        Logger.error("#{__MODULE__}: failed to create table for instance " <>
+          "#{run_opts[:instance]} (reason: #{inspect reason})")
+
+        {:error, reason}
+    end
+
   end
+
+  @impl AttributeRepository.Write
+
+  def put(resource_id, resource, run_opts) do
+    case :mnesia.transaction(fn ->
+      :mnesia.write({
+        run_opts[:instance],
+        resource_id,
+        resource,
+        now(),
+        now(),
+        nil
+      })
+    end) do
+      {:atomic, :ok} ->
+        Logger.debug("#{__MODULE__}: written #{inspect resource} " <>
+          "for instance #{run_opts[:instance]}")
+
+        {:ok, resource}
+
+      {:aborted, reason} ->
+        Logger.error("#{__MODULE__}: failed to write #{inspect resource} for instance " <>
+          "#{run_opts[:instance]} (reason: #{inspect reason})")
+
+        {:error, AttributeRepository.WriteError.exception([])}
+    end
+  end
+
+  @impl AttributeRepository.Write
+
+  def delete(resource_id, run_opts) do
+    case :mnesia.transaction(fn ->
+      :mnesia.delete({run_opts[:instance], resource_id})
+    end) do
+      {:atomic, :ok} ->
+        Logger.debug("#{__MODULE__}: deleted #{inspect resource_id} " <>
+          "for instance #{run_opts[:instance]}")
+
+        :ok
+
+      {:aborted, reason} ->
+        Logger.error("#{__MODULE__}: failed to delete #{inspect resource_id} for instance " <>
+          "#{run_opts[:instance]} (reason: #{inspect reason})")
+
+        {:error, AttributeRepository.WriteError.exception([])}
+    end
+  end
+
+  defp now(), do: System.system_time(:second)
 end
